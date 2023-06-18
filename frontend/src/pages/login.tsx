bd765aef5c05a96cce05f83a314794e8a4a77666
AharenPranata <comand.alvin@gmail.com>
2023-06-18 08:54:15 +0700

    testing

--- frontend/src/pages/login.tsx
+++ frontend/src/pages/login.tsx
@@ -1,63 +1,54 @@
-import { useRef, useState } from "react"
-import {useAuth} from "@/hooks/useAuth";
-import AlertDialog from "@/components/AlertDialog";
-import { useRouter } from "next/router";
-import Cookies from "js-cookie"
-
-
-
-async function getCsrf() {
-    const _csrf = await fetch(`${process.env.BASE_URL}/api/_get_csrf/`, {method: 'GET'})
-
-    // console.log(_csrf.headers)
-  
-}
+import Head from "next/head";
+import Link from "next/link";
 
 
 export default function Login() {
-    
-
-    // getCsrf()
-
-
-    const usernameRef = useRef<HTMLInputElement>();
-    const passwordRef = useRef<HTMLInputElement>();
-    const [errorMessege, setErrorMessege] = useState({type: '', msg: ''})
-    const router = useRouter()
-
-    const authHandler = useAuth(setErrorMessege, router);
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        authHandler(usernameRef, passwordRef)
-    }
 
 
     return (
-        <div className="absolute left-1/2 -translate-x-1/2 rounded-md flex h-screen min-w-max">
-            {errorMessege.type  && <AlertDialog type={errorMessege.type} msg={errorMessege.msg}/>}
+        <>
+            <Head>
+                <title>Masuk - Preview</title>
+            </Head>
+            <style jsx global>{`
+                body {
+                    background: linear-gradient(115.81deg, #40A763 3.47%, rgba(153, 255, 222, 0.95) 57.43%, #007952 105.85%);
+                    padding: 20px 0;
+                    box-sizing: border-box;
+                    display: grid;
+                    grid-place-items: center;
+                }
+            `}</style>
+            <div className="absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 px-6 md:px-2 rounded-md flex h-max min-w-max bg-white">
 
-            <div className="flex flex-col items-center h-full py-16">
-                <h3 className="text-3xl xl:text-4xl font-bold mb-[50px] px-12">Selamat Datang</h3>
-                <img src="logo.png" alt="logo-uim" className="max-w-[150px]" />
+                <div className="bg-red-500 w-full">
+                    hello
+                </div>
 
-                {/* form fields */}
+                <div className="flex flex-col items-center h-full py-10 min-h-full">
+                    <h3 className="text-3xl xl:text-4xl font-bold mb-[50px] px-12">Selamat Datang</h3>
+                    <img src="logo.png" alt="logo-uim" className="max-w-[150px]" />
 
-                <form onSubmit={(e) => handleSubmit(e)} method="post" action="" className="grid gap-y-14 min-w-full mt-[100px]">
-                    <div className="w-full grid gap-y-2">
-                        <label htmlFor="username" className="font-semibold">Nama Pengguna</label>
-                        <input ref={usernameRef} type="text" name="username" className="bg-slate-200 rounded-md py-2 px-4 outline-none" required/>
-                    </div>
+                    {/* form fields */}
 
-                    <div className="w-full grid gap-y-2">
-                        <label htmlFor="password" className="font-semibold">Kata Sandi</label>
-                        <input ref={passwordRef} type="password" name="password" className="bg-slate-200 rounded-md py-2 px-4 outline-none" required/>
-                    </div>
+                    <form className="grid gap-y-8 min-w-full mt-[60px]">
+                        <div className="w-full grid gap-y-2">
+                            <label htmlFor="username" className="font-semibold">Nama Pengguna</label>
+                            <input type="text" name="username" className="bg-slate-200 rounded-md py-2 px-4 outline-none" required />
+                            <Link href="" className="text-sm text-blue-500 text-end font-semibold hover:underline">Belum daftar?</Link>
+                        </div>
 
-                    <button  type="submit" className="w-full py-2 bg-base rounded-md font-semibold text-white">Masuk</button>
-                </form>
+                        <div className="w-full grid gap-y-2">
+                            <label htmlFor="password" className="font-semibold">Kata Sandi</label>
+                            <input type="password" name="password" className="bg-slate-200 rounded-md py-2 px-4 outline-none" required />
+                            <Link href="" className="text-sm text-blue-500 text-end font-semibold hover:underline">Lupa Password?</Link>
+                        </div>
+
+                        <button type="submit" className="w-full py-2 bg-base rounded-md font-semibold text-white mt-[20px]">Masuk</button>
+                    </form>
+                </div>
+
             </div>
-
-        </div>
+        </>
     )
 }
