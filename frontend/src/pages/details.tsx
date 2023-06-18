184244cd36478efff12b6c99824213662e4e61ac
AharenPranata <comand.alvin@gmail.com>
2023-06-18 17:00:14 +0700

    redirection problem

---
+++ frontend/src/pages/details.tsx
@@ -0,0 +1,62 @@
+import Home from "@/layouts/Home"
+import { useState } from "react"
+
+
+function Details() {
+    const [revealed, setRevealed] = useState(false)
+
+    return (
+        
+        <div className="py-6 px-4 max-h-screen">
+        <table className="min-w-full border-separate border-spacing-8">
+            <thead className="hidden md:table-header-group">
+                <tr className="flex py-4 px-6 justify-between min-w-ful rounded-3xl md:rounded-none md:table-row md:p-0">
+                    <th >Mata Kuliah</th>
+                    <th>Dosen Pengajar</th>
+                    <th>Bobot</th>
+                    <th>Status Kelulusan</th>
+                </tr>
+            </thead>
+            <tbody className="min-w-full flex flex-col gap-6 md:table-row-group">
+                <tr className="flex py-4 px-6 justify-between min-w-full bg-slate-300 md:bg-white rounded-3xl md:rounded-none md:table-row">
+                    <td className="text-center">Pemograman Dasar</td>
+                    <td className="hidden md:table-cell text-center">Moh Hamzah</td>
+                    <td className="hidden md:table-cell text-center">20</td>
+                    <td className="hidden md:table-cell text-center font-bold text-red-500">TIDAK LULUS</td>
+                    <td>
+                        <button className="md:hidden" onClick={() => setRevealed(!revealed)}>{revealed ? 'Hide' : 'Expand'}</button>
+                    </td>
+                </tr>
+                <tr className={`${revealed ? 'table-row' : 'hidden'} md:hidden`}>
+                    <td colSpan={2} className="flex px-4 py-2">
+                        <div className="grid w-full gap-4">
+                            <div className="flex min-w-full justify-between">
+                                <p>Nama Pengajar:</p>
+                                <p>Moh Amin</p>
+                            </div>
+                            <div className="flex min-w-full justify-between">
+                                <p>Bobot</p>
+                                <p>20</p>
+                            </div>
+                            <div className="flex min-w-full justify-between">
+                                <p>Status Kelulusan</p>
+                                <p className="font-bold text-red-500">TIDAK LULUS</p>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    )
+}
+
+export default Details
+
+Details.getLayout = function getLayout(page) {
+    return (
+        <Home>
+            {page}
+        </Home>
+    )
+}
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,24 +1,13 @@
-import Sidebar from '@/components/Sidebar'
-// import { tokenChekcer } from '@/hooks/useAuth'
-import Head from 'next/head'
-import Home from '@/layouts/Home'
 
+export function getInitialProps({res}) {
+  res.redirect("details")
+} 
 
 export default function Index() {
+
   return (
     <>
-    <Head>
-      <title>Dashboard - Preview</title>
-      <meta name="description" content="Simat UIM Preview" />
-      <meta charSet="utf-8" />
-      <meta name="author" content="Alvin Setya Pranata" />
-      <meta name="keywords" content="simat, uim" />
-    </Head>
-    
-    <main className='w-full max-h-screen flex'>
-      <Sidebar />
-      <Home />
-    </main>
+   
     </>
   )
 }
