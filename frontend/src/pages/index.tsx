ec8092b93eb7ce8fa64ae5781a3f112b1d3388c1
AharenPranata <comand.alvin@gmail.com>
2023-06-18 09:15:25 +0700

    p

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,39 +1,21 @@
 import Sidebar from '@/components/Sidebar'
 // import { tokenChekcer } from '@/hooks/useAuth'
-import Home from '@/layouts/Home'
 import Head from 'next/head'
-import { useRouter } from 'next/router'
-import { InferGetServerSidePropsType } from 'next'
-import Cookies from 'js-cookie'
-
-
-export async function getData() {
-  const data = await fetch(`${process.env.BASE_URL}/api/user/`, {
-    method: 'GET',
-    headers: {
-      'X-CSRFToken' : Cookies.get('csrftoken'),
-      'Authorization' : `JWT ${Cookies.get('access')}`,
-      'Content-Type' : 'application/json'
-    }
-  })
-
-  return data.json()
-}
+import Home from '@/layouts/Home'
 
 
 export default function Index() {
-  
-  // if token is expired or missing
-  // then it will navigate to login
-  const data = getData()
-
   return (
     <>
     <Head>
-      <title>Dashboard</title>
+      <title>Dashboard - Preview</title>
+      <meta name="description" content="Simat UIM Preview" />
+      <meta charSet="utf-8" />
+      <meta name="author" content="Alvin Setya Pranata" />
+      <meta name="keywords" content="simat, uim" />
     </Head>
     
-    <main className='w-max h-max flex'>
+    <main className='w-full max-h-screen flex'>
       <Sidebar />
       <Home />
     </main>
